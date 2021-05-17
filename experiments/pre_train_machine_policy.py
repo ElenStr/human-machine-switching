43f9df61384973edec0bcd5d71aac18b6251c1f9
Elen <helstrait@gmail.com>
2021-05-17 14:31:55 +0300

    fully automated algorithm training

--- experiments/pre_train_machine_policy.py
+++ experiments/pre_train_machine_policy.py
@@ -1,39 +1,46 @@
 import datetime
 import random
-from copy import copy, deepcopy
-
-from agent.agents import OptimalAgent, UCRL2, Agent, UniformDriverAgent
-from environments.env import Environment, GridWorld
-from environments.make_envs import HEIGHT, WIDTH
-
-from definitions import ROOT_DIR
-
 import numpy as np
 import pickle
 
-from experiments.utils import evaluate
+from definitions import ROOT_DIR
+from experiments.utils import learn_off_policy, learn_evaluate
+from agent.agents import Agent
+
+def save_agent_cost(agent, costs, on_off):
+    with open(ROOT_DIR + '/outputs/agents/pre_trained_machine/machine_agent_'+on_off, 'wb') as file:
+        pickle.dump(agent, file, pickle.HIGHEST_PROTOCOL)
+
+    with open(ROOT_DIR + '/outputs/agents/pre_trained_machine/machine_costs_'+on_off, 'wb') as file:
+        pickle.dump(costs, file, pickle.HIGHEST_PROTOCOL)
 
 
-
-
-
-
-def run_pre_train_machine(env: Environment, mdp: EpisodicMDP, n_episode: int, verbose: bool = True,
-                          save_agent: bool = True):
+def run_pre_train_machine(switching_agent: Agent, agents,
+                          trajectories, env_generator, n_episode_on: int,
+                          verbose: bool = True, save_agent: bool = True):
     """
-    Pre-train a machine policy in `no-car` subspace of the lane driving env. In each episode, 
-    it generates a new trajectory of the lane driving env and runs UCRL2 to find the switching
-    policy. To train the agent, we only run once in an episode. However, the evaluation is done 
-    by repeating the experiment `n_try` times in each episode.
+    Train the machine policy for fully automated algorithm and the fixed 
+    agent policies case
 
     Parameters
     ---------
-    env: Environment
+    switching_agent: Agent
+        The fixed switching agent for machine
 
-    mdp: EpisodicMDP
-        The mdp in which we train the machine policy
+    agents: List of Agent 
+        The machine agent to be trained, and the human agent
+        that acted for the trajectories gathering (never to take action
+        just needed for the human policy approximation).
 
-    n_episode: int
+    trajecotries: List of List of tuples 
+        The trajectories induced by the human acting alone, 
+        needed for the off-policy stage.
+
+    env_generator: lambda: environment.generate_gridworld(args)
+        The gridworld generator for the on-policy stage
+    
+    n_episode_on: int
+        Number of episodes in the on policy stage
 
     verbose : bool
         If `True`, then it will print logs every 1000 episodes
@@ -43,46 +50,45 @@ def run_pre_train_machine(env: Environment, mdp: EpisodicMDP, n_episode: int, ve
 
     Returns
     -------
-    ucrl2_regret : list
-        A list containing the regret of UCRL2 algorithm in each episode
-    machine_policy: Agent
-        The pre-trained machine policy
+    (switching, machine): tuple of Agent
+        The pre-trained machine agent 
+
+    machine_cost : list
+        A list containing the cost of the machine agent in each episode
     """
+    machine_costs = []
+    machine = agents[1]
 
-    optimal_agent = OptimalAgent(mdp=mdp)
 
-    ucrl2_agent = UCRL2(env=mdp, delta=0.1, scale=0.1)
-    ucrl2_regret = []
-
-    feat_ext = make_state_extractor(env)
-
-    for ep in range(n_episode):
-        ucrl2_agent.scale = max(0.1 - (ep // (n_episode / 10)) * 0.01, 0)
-        ucrl2_agent.update_policy(ep)
-        
-        # initialize a new grid trajectory for this episode with `no-car` initial traffic level
-        grid_env = Environment().generate_grid_world(width=WIDTH, height=HEIGHT, init_traffic_level='no-car')
-
-        # evaluate the optimal policy
-        optimal_value = evaluate(agent=optimal_agent, env=grid_env, feat_ext=feat_ext)
-
-        # evaluate and train ucrl2
-        value = evaluate(agent=ucrl2_agent, env=grid_env, feat_ext=feat_ext)
-        ucrl2_regret.append(value - optimal_value)
+    for ep,traj in enumerate(trajectories):
+        ep_cost = learn_off_policy(switching_agent, agents, traj)
+        machine_costs.append(ep_cost)
 
         # print log
-        if verbose and ep % 1000 == 0:
-            print(f'{datetime.datetime.now()}, Episode {ep}, UCRL2 algorithm cumulative regret: {np.sum(ucrl2_regret)}')
+        if verbose and ep % 1000 == 0 and (ep // 1000 > 0):
+            print(f'{datetime.datetime.now()}, Episode {ep}, Fully automated off-policy algorithm cumulative cost: {np.sum(machine_costs)}')
 
         # save agent
         if save_agent and (ep % 1000 == 0) and (ep // 1000 > 0):
-            with open(ROOT_DIR + '/outputs/agents/pre_trained_machine/machine_agent', 'wb') as file:
-                pickle.dump(ucrl2_agent, file, pickle.HIGHEST_PROTOCOL)
+            save_agent_cost(machine, machine_costs, 'off')
+        
+    for ep in range(n_episode_on):
+        grid_world = env_generator()
+        ep_cost = learn_evaluate(switching_agent, agents, grid_world, is_learn=True)
+        machine_costs.append(ep_cost)
 
-            with open(ROOT_DIR + '/outputs/agents/pre_trained_machine/machine_regret', 'wb') as file:
-                pickle.dump(ucrl2_regret, file, pickle.HIGHEST_PROTOCOL)
+        # print log
+        if verbose and ep % 1000 == 0 and (ep // 1000 > 0):
+            print(f'{datetime.datetime.now()}, Episode {ep}, Fully automated on-policy algorithm cumulative cost: {np.sum(machine_costs)}')
 
-    ucrl2_agent.final_update()
-    with open(ROOT_DIR + '/outputs/agents/pre_trained_machine/machine_agent', 'wb') as file:
-        pickle.dump(ucrl2_agent, file, pickle.HIGHEST_PROTOCOL)
-    return ucrl2_agent, ucrl2_regret
+        # save agent
+        if save_agent and (ep % 1000 == 0) and (ep // 1000 > 0):
+            save_agent_cost(machine, machine_costs, 'on')
+
+    
+    
+    
+    switching_agent.trainable = False
+    machine.trainable = False
+    # make sure machine changes
+    return switching_agent, agents, machine_costs
