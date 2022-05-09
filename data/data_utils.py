1ec18b5f4c9584974bc4e632d43be5ed6ff9c3c8
el <some@some>
2022-05-09 19:46:19 +0200

    clean up trips missing nodes, multi finish.

--- data/data_utils.py
+++ data/data_utils.py
@@ -1,6 +1,7 @@
 from definitions import ROOT_DIR
 from csv import reader as csv_reader
 import osmnx as ox
+from copy import deepcopy
 
 def get_lat_lon(graph, node_number):
     node_dict = graph.nodes[node_number]
@@ -9,7 +10,6 @@ def get_lat_lon(graph, node_number):
 
 def get_distance(graph, node_id_u, node_id_v):
     """Haversine distance between two nodes"""
-    assert not( node_id_u == 3 or node_id_v ==3)
         
     u_coords =  get_lat_lon(graph, node_id_u)
     v_coords =  get_lat_lon(graph, node_id_v)
@@ -22,14 +22,13 @@ def get_distance(graph, node_id_u, node_id_v):
 def trips_mutli_finish(processed_trip_path):
     with open(processed_trip_path, 'r') as f:
         reader = csv_reader(f)
-        trip_id = 0
         trips_mfinish = []
         all_trips = []
         check = False
         for i,row in enumerate(reader):
             # if finished
             if row[2] == 'True':
-                trip_id = row[4]
+                trip_id = float(row[4])
                 # First trip that finishes
                 if not len(all_trips):
                     # Check if it finishes again
@@ -49,20 +48,56 @@ def trips_mutli_finish(processed_trip_path):
 
 
 
-def fill_graph(processed_trip_path, graph, ref_graph):
+def trips_with_missing_nodes(processed_trip_path, proc_graph, ref_graph):
+    graph = deepcopy(proc_graph)
     with open(processed_trip_path, 'r') as f:
         reader = csv_reader(f)
-        k = 0
         trips_to_exclude = []
-        trip_id_in = False
+        nodes_to_exclude = []
         for i,row in enumerate(reader):
             if i > 0:
-                trip_id = row[4]
+                trip_id = float(row[4])
                 u = int(row[0])
                 v = int(row[1])
-                    
-                assert not( u == 3 or v ==3)
+                if (not len(trips_to_exclude)) or trip_id!=trips_to_exclude[-1]:      
+                    for node in [u,v]:
+                        try:                
+                            graph.nodes[node]
+                        except KeyError:
+                            try:
+                                ref_graph.nodes[node]
+                                graph.add_node(node)
+                                for key,val in ref_graph.nodes[node].items():
+                                    graph.nodes[node][key] = val
+                                
+                                if not graph.has_edge(u,v):
+                                    length = row[3]
+                                    if length == '':
+                                        length = get_distance(ref_graph,u,v)
+                                    else:
+                                        length = float(length)
+                                        
+                                    graph.add_edge(u,v, length=length)
+                            except KeyError:
+                                if (not len(trips_to_exclude)) or trip_id!=trips_to_exclude[-1]:
+                                    trips_to_exclude.append(trip_id)
+                                    nodes_to_exclude.append(node)
+                                    
+                                   
 
+                        
+        return trips_to_exclude,list(set(nodes_to_exclude))
+                    
+def graph_from_trips(trips_path, graph, ref_graph):
+    """Return a graph that corresponds to the trips_path"""
+    with open(trips_path, 'r') as f:
+        reader = csv_reader(f)
+        for i,row in enumerate(reader):
+            if i > 0:
+                trip_id = float(row[4])
+                u = int(row[0])
+                v = int(row[1])
+                
                 for node in [u,v]:
                     try:                
                         graph.nodes[node]
@@ -72,26 +107,27 @@ def fill_graph(processed_trip_path, graph, ref_graph):
                             graph.add_node(node)
                             for key,val in ref_graph.nodes[node].items():
                                 graph.nodes[node][key] = val
-                            trip_id_in = False
-                            if not graph.has_edge(u,v):
-                                if not trip_id_in:
-                                    length = row[3]
-                                    if length == '':
-                                        length = get_distance(ref_graph,u,v)
-                                    else:
-                                        length = float(length)
-                                        
-                                    graph.add_edge(u,v, length=length)
+                            
                         except KeyError:
-                            if (not len(trips_to_exclude)) or trip_id!=trips_to_exclude[-1]:
-                                trips_to_exclude.append(trip_id)
-                                trip_id_in= True
+                            graph.remove_node(node)
+                    try:
+                        if not graph.has_edge(u,v):
+                            length = row[3]
+                            if length == '':
+                                length = get_distance(ref_graph,u,v)
+                            else:
+                                length = float(length)
+                                
+                            graph.add_edge(u,v, length=length)
+                    except KeyError:
+                        graph.remove_node(u)
+                        graph.remove_node(v)
+            
+    return graph
 
-                     
-        return trips_to_exclude,graph
-                    
 
-                   
+                        
+
                 
 def add_trip_ids_to_nodes(processed_trip_path,graph):
     # initialize trip id lists
@@ -103,7 +139,7 @@ def add_trip_ids_to_nodes(processed_trip_path,graph):
         k = 0
         for i,row in enumerate(reader):
             if i > 0:
-                trip_id = row[4]
+                trip_id = float(row[4])
                 u = int(row[0])
                 v = int(row[1])
                 
@@ -116,7 +152,8 @@ def add_trip_ids_to_nodes(processed_trip_path,graph):
     return graph
 
 
-def trips_dead_ends(graph):
+def trips_with_dead_ends(valid_graph):
+    graph = deepcopy(valid_graph)
     done = False
     trips_with_dead_ends = []
     while not done: 
--- data/preprocess_data.py
+++ data/preprocess_data.py
@@ -3,24 +3,21 @@ from data.data_utils import *
 from definitions import ROOT_DIR
 
 
-def trips_to_remove_save_filled_graph(trips_path, graph_path):
+
+def corrupted_trips(trips_path, G, ref_G):
     trips_m_finish = trips_mutli_finish(trips_path)
-    G = ox.graph_from_xml(graph_path)
-    ref_G = ox.graph_from_place('Distrito do Porto, PT', clean_periphery=False)
+    
 
-    trips_with_unknown_nodes,G = fill_graph(trips_path, G, ref_G)
+    trips_missing_nodes, missing_nodes = trips_with_missing_nodes(trips_path, G, ref_G)
 
-    ox.io.save_graphml(G, filepath=f"{ROOT_DIR}/data/filled_graph_porto.osm")
-    G = add_trip_ids_to_nodes(trips_path,G)
+    trips_to_remove = sorted(trips_m_finish + trips_missing_nodes)
+ 
 
-
-    trips_with_dead_ends,G = list(set(trips_dead_ends(G)))
-
-    return trips_m_finish+trips_with_unknown_nodes+trips_with_dead_ends
+    return set(trips_to_remove)
 
 
-def write_valid_trips(new_trips_path, old_trips_path, graph_path):
-    trips_to_rm = sorted(trips_to_remove_save_filled_graph(old_trips_path, graph_path))
+def write_valid_trips(new_trips_path, old_trips_path, graph, ref_graph, valid_fn):
+    trips_to_rm = valid_fn(old_trips_path, graph, ref_graph)
     j = 0
     id_rm_start = False
     # id_rm_finish = False
@@ -31,12 +28,12 @@ def write_valid_trips(new_trips_path, old_trips_path, graph_path):
 
             for i,row in enumerate(reader):
                 if i > 0:
-                    trip_id = row[4]
+                    trip_id = float(row[4])
                     if trip_id != trips_to_rm[j]:
                         if not id_rm_start:
                             writer.writerow(row)
                         else:
-                            if j < len(trips_to_rm):
+                            if j < len(trips_to_rm)-1:
                                 if trip_id != trips_to_rm[j+1]:
                                     id_rm_start = False 
                                     writer.writerow(row)
@@ -54,7 +51,79 @@ def write_valid_trips(new_trips_path, old_trips_path, graph_path):
                         else:
                             pass
                 else:
-                    pass
+                    # write header
+                    writer.writerow(row)
+
+
+
+if '__name__'=='__main__':
+    trips_no_mfinish_no_missing_nodes = './data/no_mfin_no_mis_nodes.csv'
+    old_trips = './processed_trips.csv'
+    new_trips_path='data/cleaned_up_trips.csv'
+    graph_path = 'data/Porto_driving.osm'
+    G = ox.graph_from_xml(graph_path)
+    ref_G = ox.graph_from_place('Distrito do Porto, PT', clean_periphery=False,network_type='drive')
+
+
+    write_valid_trips(new_trips_path=trips_no_mfinish_no_missing_nodes, old_trips_path=old_trips, graph=G, ref_graph=ref_G, valid_fn=corrupted_trips)
+
+    graph_with_no_missing_nodes = graph_from_trips(trips_no_mfinish_no_missing_nodes,G, ref_G)
+
+    write_valid_trips(new_trips_path,trips_no_mfinish_no_missing_nodes,graph=graph_with_no_missing_nodes)
+
+
+# def trips_to_remove_save_filled_graph(trips_path, graph_path):
+#     trips_m_finish = trips_mutli_finish(trips_path)
+#     G = ox.graph_from_xml(graph_path)
+#     ref_G = ox.graph_from_place('Distrito do Porto, PT', clean_periphery=False,network_type='drive')
+
+#     trips_with_unknown_nodes,G = fill_graph(trips_path, G, ref_G)
+
+#     ox.io.save_graphml(G, filepath=f"{ROOT_DIR}/data/filled_graph_porto.osm")
+#     G = add_trip_ids_to_nodes(trips_path,G)
+
+
+#     trips_with_dead_ends = list(set(trips_dead_ends(G)))
+
+#     return trips_m_finish+trips_with_unknown_nodes+trips_with_dead_ends
+
+
+# def write_valid_trips(new_trips_path, old_trips_path, graph_path):
+#     trips_to_rm = sorted(trips_to_remove_save_filled_graph(old_trips_path, graph_path))
+#     j = 0
+#     id_rm_start = False
+#     # id_rm_finish = False
+#     with open(new_trips_path, 'w') as fw:
+#         writer = csv_writer(fw)
+#         with open(old_trips_path, 'r') as fr:
+#             reader = csv_reader(fr)
+
+#             for i,row in enumerate(reader):
+#                 if i > 0:
+#                     trip_id = row[4]
+#                     if trip_id != trips_to_rm[j]:
+#                         if not id_rm_start:
+#                             writer.writerow(row)
+#                         else:
+#                             if j < len(trips_to_rm):
+#                                 if trip_id != trips_to_rm[j+1]:
+#                                     id_rm_start = False 
+#                                     writer.writerow(row)
+#                                 else:
+#                                     pass
+#                                 j+=1
+
+#                             else:
+#                                 id_rm_start = False 
+#                                 writer.writerow(row)
+
+#                     else:
+#                         if not id_rm_start:
+#                             id_rm_start = True
+#                         else:
+#                             pass
+#                 else:
+#                     pass
                          
 
 
